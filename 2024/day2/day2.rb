d27ff126005d44da02364cf2246098e98844714d
manny ledoux <manny.ledoux@checkr.com>
2024-12-02 07:30:33 -0500

    add: adds day 2 part 2

--- 2024/day2/day2.rb
+++ 2024/day2/day2.rb
@@ -11,20 +11,38 @@ lines = File.read('2024/day2/input.txt').split("\n")
 # Part 1 #
 ##########
 
-def is_report_safe?(report, allow_one_failure = false)
-  failures = report.each_cons(2).all? { |a, b| b > a && b - a <= 3 && b - a >= 1 }
+lines = File.read('2024/day2/input.txt').split("\n")
+
+def is_report_safe?(report)
+  ascending = report.each_cons(2).all? { |a, b| b > a && (1..3).include?(b - a) }
+
+  descending = report.each_cons(2).all? { |a, b| a > b && (1..3).include?(a - b) }
+
+  ascending || descending
 end
 
-part_one = 0
-
-lines.each do |line|
+part_one_safe_count = lines.count do |line|
   report = line.split.map(&:to_i)
-  if report[0] > report[1]
-    report.reverse!
-  end
-  part_one += 1 if is_report_safe?(report)
+  is_report_safe?(report)
 end
 
-puts "Part 1: #{part_one}"
+puts "Part 1: #{part_one_safe_count}"
 
-def is_report_safe_with_dampener?(report)
+##########
+# Part 2 #
+##########
+
+def can_be_made_safe_by_one_removal?(report)
+  report.each_index.any? do |i|
+    temp_report = report.dup
+    temp_report.delete_at(i)
+    is_report_safe?(temp_report)
+  end
+end
+
+part_two_safe_count = lines.count do |line|
+  report = line.split.map(&:to_i)
+  is_report_safe?(report) || can_be_made_safe_by_one_removal?(report)
+end
+
+puts "Part 2: #{part_two_safe_count}"
