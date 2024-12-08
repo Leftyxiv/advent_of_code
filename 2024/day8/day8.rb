143bbf7eb3a9298b065843b605a74ce5cba5584c
manny ledoux <manny.ledoux@checkr.com>
2024-12-08 08:00:25 -0500

    add: adds part 2

--- 2024/day8/day8.rb
+++ 2024/day8/day8.rb
@@ -1,17 +1,7 @@
 require_relative '../../utils/griderator'
 
-# lines = """............
-# ........0...
-# .....0......
-# .......0....
-# ....0.......
-# ......A.....
-# ............
-# ............
-# ........A...
-# .........A..
-# ............
-# ............""".split("\n")
+# lines = File.read('2024/day8/sample.txt').split("\n")
+# lines = File.read('2024/day8/sample2.txt').split("\n")
 lines = File.read('2024/day8/input.txt').split("\n")
 
 grid = Griderator5000.new(lines)
@@ -19,11 +9,13 @@ grid = Griderator5000.new(lines)
 nodes = grid.find_all_not_locations('.')
 
 node_store = {}
+node_list = []
 stupid_jammer_antenna = []
 
 nodes.each do |node|
   node_store[grid[node[0], node[1]]] ||= []
   node_store[grid[node[0], node[1]]] << node
+  node_list << node
 end
 ##########
 # Part 1 #
@@ -31,7 +23,6 @@ end
 
 
 node_store.each do |key, value|
-  
   value.each_with_index do |node, index|
     loop_count = 0
     loop do
@@ -44,14 +35,41 @@ node_store.each do |key, value|
       stupid_jammer_antenna << anti_node_two if grid.in_bounds?(anti_node_two[0], anti_node_two[1])
       loop_count += 1
     end
-    # next if index == value.length - 1
-
-    # diff = grid.get_difference(value[index], value[index + 1])
-    # anti_node_one = [value[index + 1][0] + diff[0], value[index + 1][1] + diff[1]]
-    # anti_node_two = [node[0] - diff[0], node[1] - diff[1]]
-    # puts [node.inspect, value[index + 1].inspect, anti_node_one.inspect, anti_node_two.inspect].inspect
-    # anti_node_locs << anti_node_one
-    # anti_node_locs << anti_node_two
   end
 end
+
 puts "Part 1: #{stupid_jammer_antenna.uniq.size}"
+
+##########
+# Part 2 #
+##########
+
+stupid_jammer_antenna_on_steroids = []
+
+node_store.each do |key, value|
+  value.each_with_index do |node, index|
+    loop_count = 0
+    loop do
+      break if loop_count == value.length - 1 - index
+
+      diff = grid.get_difference(value[index], value[index + 1 + loop_count])
+      anti_node_one = [value[index + 1 + loop_count][0] + diff[0], value[index + 1 + loop_count][1] + diff[1]]
+      anti_node_two = [node[0] - diff[0], node[1] - diff[1]]
+      anti_node_one_multiplier = 2
+      anti_node_two_multiplier = 2
+      while grid.in_bounds?(anti_node_one[0], anti_node_one[1]) do
+        stupid_jammer_antenna_on_steroids << anti_node_one
+        anti_node_one = [value[index + 1 + loop_count][0] + diff[0] * anti_node_one_multiplier, value[index + 1 + loop_count][1] + diff[1] * anti_node_one_multiplier]
+        anti_node_one_multiplier += 1
+      end
+      while grid.in_bounds?(anti_node_two[0], anti_node_two[1]) do
+        stupid_jammer_antenna_on_steroids << anti_node_two
+        anti_node_two = [node[0] - diff[0] * anti_node_two_multiplier, node[1] - diff[1] * anti_node_two_multiplier]
+        anti_node_two_multiplier += 1
+      end
+      loop_count += 1
+    end
+  end
+end
+
+puts  "Part 2: #{stupid_jammer_antenna_on_steroids.concat(node_list).uniq.size}"
